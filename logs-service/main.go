c9f7108aec53f9eec68d92681a0fa860f05eaaf2
jerryben <onwohjeremiah@gmail.com>
2025-04-04 20:30:56 +0100

    Adjusted modeule paths

--- logs-service/main.go
+++ logs-service/main.go
@@ -9,25 +9,24 @@ import (
     "github.com/jerryben/DevOps-Monitoring-Microservices/logs-service/app/routes"
 )
 
-
 func main() {
-	// Load environment variables
-	godotenv.Load()
+    // Load environment variables
+    godotenv.Load()
 
-	// Initialize database
-	database.ConnectDB()
+    // Initialize database
+    database.ConnectDB()
 
-	// Create Fiber instance
-	app := fiber.New()
+    // Create Fiber instance
+    app := fiber.New()
 
-	// Setup routes
-	routes.SetupRoutes(app)
+    // Setup routes
+    routes.SetupRoutes(app)
 
-	// Start the server
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "5001"
-	}
+    // Start the server
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "5001"
+    }
 
-	log.Fatal(app.Listen(":" + port))
+    log.Fatal(app.Listen(":" + port))
 }
