a57435a5623604dcddefe7ce19a2b9384bd4d39b
jerryben <onwohjeremiah@gmail.com>
2025-04-18 00:13:57 +0100

    Set net to false in config-override

--- services/visualization-ui/frontend/config-overrides.js
+++ services/visualization-ui/frontend/config-overrides.js
@@ -9,7 +9,8 @@ module.exports = function override(config) {
     buffer: require.resolve('buffer'),
     process: require.resolve('process/browser'),
     querystring: require.resolve('querystring-es3'), // Polyfill for 'querystring'
-    path: require.resolve('path-browserify') // Polyfill for 'path'
+    path: require.resolve('path-browserify'), // Polyfill for 'path'
+    net: false, // Disable 'net' as it is not needed in the browser
   };
 
   config.plugins = (config.plugins || []).concat([
