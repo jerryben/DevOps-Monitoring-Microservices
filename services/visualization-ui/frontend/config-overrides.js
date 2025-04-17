208093f5344e552661d7f950726c9a22360f8e51
jerryben <onwohjeremiah@gmail.com>
2025-04-17 20:51:39 +0100

    Added querystring-es3

---
+++ services/visualization-ui/frontend/config-overrides.js
@@ -0,0 +1,10 @@
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    querystring: require.resolve('querystring-es3'),
+  };
+
+  return config;
+};
