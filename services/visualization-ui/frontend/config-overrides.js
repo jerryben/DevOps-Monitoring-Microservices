8f27e996b49f867a4fb0d8df56205f5e8051b7a1
jerryben <onwohjeremiah@gmail.com>
2025-04-17 23:30:40 +0100

    added querystring-es3 to config-override

--- services/visualization-ui/frontend/config-overrides.js
+++ services/visualization-ui/frontend/config-overrides.js
@@ -3,16 +3,18 @@ const webpack = require('webpack');
 module.exports = function override(config) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
-    http: require.resolve('stream-http'), // Add polyfill for 'http'
-    https: require.resolve('https-browserify'), // Add polyfill for 'https' if needed
-    stream: require.resolve('stream-browserify'), // Add polyfill for 'stream'
-    buffer: require.resolve('buffer'), // Add polyfill for 'buffer'
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    stream: require.resolve('stream-browserify'),
+    buffer: require.resolve('buffer'),
+    process: require.resolve('process/browser'),
+    querystring: require.resolve('querystring-es3'), // Add polyfill for 'querystring'
   };
 
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'], // Provide Buffer globally
-      process: 'process/browser', // Provide process globally
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser',
     }),
   ]);
 
