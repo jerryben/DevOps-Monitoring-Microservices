96116076fea303e369841bbc5d81e605caa0aaf5
jerryben <onwohjeremiah@gmail.com>
2025-04-17 22:07:05 +0100

    Update package.json

--- services/visualization-ui/frontend/config-overrides.js
+++ services/visualization-ui/frontend/config-overrides.js
@@ -3,8 +3,18 @@ const webpack = require('webpack');
 module.exports = function override(config) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
-    querystring: require.resolve('querystring-es3'),
+    http: require.resolve('stream-http'), // Add polyfill for 'http'
+    https: require.resolve('https-browserify'), // Add polyfill for 'https' if needed
+    stream: require.resolve('stream-browserify'), // Add polyfill for 'stream'
+    buffer: require.resolve('buffer'), // Add polyfill for 'buffer'
   };
+
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'], // Provide Buffer globally
+      process: 'process/browser', // Provide process globally
+    }),
+  ]);
 
   return config;
 };
